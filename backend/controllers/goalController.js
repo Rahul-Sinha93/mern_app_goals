c48a6cda058a7be413f947e4fe83bf88234260f3
Rahul <rahulsinhar93@gmail.com>
2022-03-10 17:34:32 -0500

    Added auth reducer and hooked up the login and Register form to state

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,16 +40,14 @@ const updateGoal = asyncHandler( async(req,res) => {
         throw new Error('Goal not Found')
     }
 
-    const user = await User.findById(req.user.id);
-
     //check for user 
-    if(!user) {
+    if(!req.user) {
         res.status(401);
         throw new Error('User not Found')
     }
 
     //Make sure the logged in user matches
-    if(goal.user.toString() !== user.id ){
+    if(goal.user.toString() !== req.user.id ){
         res.status(401);
         throw new Error('User not authorized')
     }
@@ -71,15 +69,14 @@ const deleteGoal = asyncHandler( async (req,res) => {
         throw new Error('Goal not Found')
     }
 
-    const user = await User.findById(req.user.id);
     //check for user 
-    if(!user) {
+    if(!req.user) {
         res.status(401);
         throw new Error('User not Found')
     }
 
     //Make sure the logged in user matches
-    if(goal.user.toString() !== user.id ){
+    if(goal.user.toString() !== req.user.id ){
         res.status(401);
         throw new Error('User not authorized')
     }
